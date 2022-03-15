4a6e8c27dd15e2adfb7899911ecc8072d697b236
Ali Shanks <a.shanks@live.com>
2022-03-15 21:06:33 +0000

    edited character controller script slightly to add flip feature

--- GJL_Spring22_GameJam/Assets/Scripts/TopDownCharacterController.cs
+++ GJL_Spring22_GameJam/Assets/Scripts/TopDownCharacterController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class TopDownCharacterController : MonoBehaviour
 {
+
+    public static TopDownCharacterController instance;
+
+
     private Rigidbody2D myRigidbody2D;
     private Vector3 moveDir;
-    public Animator animator;
 
+
+    public Animator animator;
+    public bool isFacingRight = true;
     public float MOVE_SPEED = 20f;
 
     private void Awake()
     {
+        instance = this;
+        
         myRigidbody2D = GetComponent<Rigidbody2D>();
 
     }
@@ -29,16 +37,29 @@ public class TopDownCharacterController : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             moveY = -1f;
+
+            
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             moveX = -1f;
+
+            if (isFacingRight)
+            {
+                Flip();
+            }
+
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             moveX = +1f;
+
+            if (!isFacingRight)
+            {
+                Flip();
+            }
         }
 
         moveDir = new Vector3(moveX, moveY).normalized;
@@ -47,5 +68,17 @@ public class TopDownCharacterController : MonoBehaviour
     private void FixedUpdate()
     {
         myRigidbody2D.velocity = moveDir * MOVE_SPEED;
+    }
+
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        isFacingRight = !isFacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
